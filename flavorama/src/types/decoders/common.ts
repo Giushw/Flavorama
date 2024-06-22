a81fc55b3e57747df5c2184b9b0c517d0e4784a8
Giuseppe Dell'Avvocato <dellavvocatog@gmail.com>
2024-06-22 18:57:47 +0200

    chore(): added some Api response decs.

--- flavorama/src/types/decoders/common.ts
+++ flavorama/src/types/decoders/common.ts
@@ -7,3 +7,39 @@ interface Type {
 
 // List of entities for cuisines | difficulties | diets
 export type TypesList = Iterable<Type>;
+
+/* Cuisines
+[
+  {"id": "1", "name": "Italian"},
+  {"id": "2", "name": "American"},
+  {"id": "3", "name": "Mexican"},
+  {"id": "4", "name": "Indian"},
+  {"id": "5", "name": "Japanese"},
+  {"id": "6", "name": "Spanish"},
+  {"id": "7", "name": "French"},
+  {"id": "8", "name": "Greek"},
+  {"id": "9", "name": "Thai"},
+  {"id": "10", "name": "British"},
+  {"id": "11", "name": "Russian"},
+  {"id": "12", "name": "Middle Eastern"},
+  {"id": "13", "name": "North African"},
+  {"id": "14", "name": "Korean"}
+]
+ */
+
+/* Diets
+[
+  {"id": "1","name": "Vegetarian"},
+  {"id": "2", "name": "Mediterranean"},
+  {"id": "3", "name": "Non-Vegetarian"},
+  {"id": "4", "name": "Pescatarian"}
+]
+ */
+
+/* Difficulties
+[
+  {"id": "1","name": "Easy"},
+  {"id": "2", "name": "Medium"},
+  {"id": "3", "name": "Hard"}
+]
+ */
--- flavorama/src/types/decoders/recipes.ts
+++ flavorama/src/types/decoders/recipes.ts
@@ -8,7 +8,7 @@ export interface Recipe {
   cuisineId: string,
   dietId: string,
   difficultyId: string,
-  image: string, // "/uploads/carbonara-horizontal-square640-v2.jpg" // add localh 8080
+  image: string, // add localh 8080
 };
 
 export type Recipes = Iterable<Recipe>;
@@ -18,7 +18,70 @@ export interface Comment {
   recipeId: string,
   comment: string,
   rating: number // da 0 a 5
-  date: string // 2024-06-22T15:20:47.876Z
+  date: string // datestring
 };
 
 export type Comments = Iterable<Comment> ;
+
+/* Recipes
+  [
+    {
+      "id": "1",
+      "name": "Spaghetti Carbonara",
+      "ingredients": [
+        "Spaghetti",
+        "Eggs",
+        "Parmesan Cheese",
+        "Pancetta",
+        "Black Pepper"
+      ],
+      "instructions": "Boil the spaghetti. Fry the pancetta. Mix eggs and cheese. Combine all.",
+      "cuisineId": "1",
+      "dietId": "3",
+      "difficultyId": "3",
+      "image": "/uploads/carbonara-horizontal-square640-v2.jpg"
+    },
+    {
+      "id": "2",
+      "name": "Margherita Pizza",
+      "ingredients": [
+        "Pizza Dough",
+        "Tomato Sauce",
+        "Mozzarella Cheese",
+        "Basil",
+        "Olive Oil"
+      ],
+      "instructions": "Prepare the dough. Spread tomato sauce. Add cheese and basil. Bake in the oven.",
+      "cuisineId": "1",
+      "dietId": "1",
+      "difficultyId": "2",
+      "image": "/uploads/Margherita-9920.webp"
+    }
+]
+*/
+
+/* Comments
+[
+  {
+    "id": "1",
+    "recipeId": "1",
+    "comment": "Delicious and easy to make!",
+    "rating": 5,
+    "date": "2024-06-08T14:48:00.000Z"
+  },
+  {
+    "id": "2",
+    "recipeId": "1",
+    "comment": "Loved it! Perfect for a quick dinner.",
+    "rating": 4,
+    "date": "2024-06-09T12:30:00.000Z"
+  },
+  {
+    "id": "3",
+    "recipeId": "1",
+    "comment": "Great flavors, will make again.",
+    "rating": 5,
+    "date": "2024-06-10T09:45:00.000Z"
+  }
+]
+*/
