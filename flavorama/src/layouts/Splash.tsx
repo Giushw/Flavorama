3c6c7902aeff8eb3a1ae132ba9266f63fa4cfd71
Giuseppe Dell'Avvocato <dellavvocatog@gmail.com>
2024-06-23 19:58:43 +0200

    chore(): refactor code with a common layout.

---
+++ flavorama/src/layouts/Splash.tsx
@@ -0,0 +1,55 @@
+import {FC, ReactNode} from 'react';
+import {Grid} from '@mantine/core';
+import type {WiewportObj} from '@/types/commons';
+
+interface SplashProps {
+  lSize: number | WiewportObj,
+  lSlot: ReactNode,
+  rSize: number | WiewportObj,
+  rSlot: ReactNode
+};
+
+const Splash: FC<SplashProps> = ({lSize, rSize, lSlot, rSlot}) => {
+  return (
+    <Grid 
+      justify='center'
+      align='center'
+      p={50}
+      w={'100vw'}
+      h={'100vh'}
+      bg={'night.1'}
+      styles={{
+        inner: {
+          width: '100%',
+          height: '100%'
+        }
+      }}
+    >
+      <Grid.Col 
+        w={'100%'}
+        h={'100%'}
+        span={
+          typeof lSize === 'number' ?
+            {lSize} :
+            {...lSize}
+        }
+      >
+        {lSlot}     
+      </Grid.Col>
+
+      <Grid.Col 
+        w={'100%'}
+        h={'100%'}
+        span={
+          typeof rSize === 'number' ?
+            {rSize} :
+            {...rSize}
+        }
+      >
+        {rSlot}
+      </Grid.Col>
+    </Grid>
+  )
+};
+
+export default Splash;
--- flavorama/src/types/commons.ts
+++ flavorama/src/types/commons.ts
@@ -1,3 +1,12 @@
 export type Iterable<G> = G[];
 
 export type Nullable<G> = G | null;
+
+export interface WiewportObj {
+  base?: number,
+  xs?: number,
+  sm?: number,
+  md?: number,
+  lg?: number,
+  xl?: number,
+}
