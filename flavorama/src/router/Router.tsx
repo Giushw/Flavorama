9c9f1016a6fbd8dde011565cf12ada0d2c72098f
Giuseppe Dell'Avvocato <dellavvocatog@gmail.com>
2024-06-28 12:30:33 +0200

    chore(): precursor work on detail

--- flavorama/src/router/Router.tsx
+++ flavorama/src/router/Router.tsx
@@ -3,6 +3,7 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Home from '@/pages/Home';
 import Error from "@/pages/Error";
 import Search from '@/pages/Search';
+import Detail from '@/pages/Search';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: '/search',
     element: <Search />,
+  },
+  {
+    path: '/search/:id',
+    element: <Detail />,
   },
 ]);
 
